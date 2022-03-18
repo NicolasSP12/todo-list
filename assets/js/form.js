0de294bbe8d10fb574064f9cc6b399a4ed0e4d41
NicolasSP12 <nicolas_salas_1200@hotmail.com>
2022-03-18 00:33:45 -0300

    feat: Guarda datos del input

--- assets/js/form.js
+++ assets/js/form.js
@@ -1,5 +1,8 @@
 const saveButtonElement = document.querySelector("#save")
 saveButtonElement.addEventListener("click", (event) => {
     event.preventDefault()
+    let inputValue = document.querySelector("input")
+    console.log(inputValue.value)
     console.log("Guardar")
+    myLanguages.push({name: inputValue.value, complete: false, start: true})
 })
